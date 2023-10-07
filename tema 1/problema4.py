8691bcf670e88d506c3436fb8ec9ac41a4fd5bdb
Lucian <lucian_munteanu@mail.ru>
2023-10-07 14:14:15 +0300

    ex 4

---
+++ tema 1/problema4.py
@@ -0,0 +1,8 @@
+# Input text from the user
+input_text = input("Enter a string: ")
+
+# Convert uppercase letters to lowercase and replace spaces with underscores
+output_text = input_text.lower().replace(" ", "_")
+
+# Print the resulting string
+print(output_text)
